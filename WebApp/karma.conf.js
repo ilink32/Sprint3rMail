c8b68fdd570ada2d0fa7643703a11383ae0399fb
sprinter <sprinter04@sprinter04s-Mac-mini.local>
2017-08-16 13:52:14 +0700

    add jquery to karma.config.js

--- WebApp/karma.conf.js
+++ WebApp/karma.conf.js
@@ -1,6 +1,7 @@
 module.exports = function(config) {
     config.set({
         files: [
+          'bower_components/jquery/dist/jquery.js',
           'app/scripts/*.js',
           'test/spec/*.js'
         ],
