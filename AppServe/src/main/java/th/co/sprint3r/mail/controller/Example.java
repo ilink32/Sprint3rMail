58c0ef6ea78f8b657c386d6b551c68a554bbafd9
sprinter03 <sprinter03@sprinter03s-Mac-mini.local>
2017-08-16 17:34:55 +0700

    update main class update repo class

--- AppServe/src/main/java/th/co/sprint3r/mail/controller/Example.java
+++ AppServe/src/main/java/th/co/sprint3r/mail/controller/Example.java
@@ -3,7 +3,7 @@ package th.co.sprint3r.mail.controller;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"th.co.sprint3r.mail.controller","th.co.sprint3r.mail.dao","th.co.sprint3r.mail.model"})
 public class Example {
 
     public static void main(String[] args) throws Exception {
