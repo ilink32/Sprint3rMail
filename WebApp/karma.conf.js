a6362eca930fcaa47a65d765f00588031dc07eb7
maneeratsp <maneerat.sp@gmail.com>
2017-08-16 16:13:35 +0700

    create_campaign: add unit test

--- WebApp/karma.conf.js
+++ WebApp/karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
     config.set({
         files: [
           'bower_components/jquery/dist/jquery.js',
-          'app/scripts/*.js',
+          'app/scripts/create_campaign.js',
           'test/spec/*.js'
         ],
         frameworks: ['jasmine'],
--- WebApp/test/spec/test.js
+++ WebApp/test/spec/test.js
@@ -1,29 +1,41 @@
-describe("validate campaign name", function() {
 
-  it("Case Input Empty", function(){
-    window.$ = function(v) {
-      return {
-        val: function() {
-          return "";
-        }
-      }
-    }
-    var expected = "Please input campaign Name";
-    var result = validateCampaignName();
-    expect(result).toEqual(expected);
+var AuthenticationForm = function() {
+  this.txtCampaignName = $("#txtCampaignName").val();
+  this.create = function(){
+    this.lblErrorCampaignName = "Please input campaign name.";
+    $("#lblErrorCampaignName").text(this.lblErrorCampaignName);
+  }
+};
+
+
+describe("Validate campaign name", function() {
+  
+  
+  
+  var rootId = 'testContainer';
+  var authenticationFormMarkup = '<form>'+
+    '<input id="txtCampaignName" type="text" placeholder="" maxlength="50">'+
+    '<button id="btnCreateCampaign" type="button" >Create</button>'+
+    '<span id="lblErrorCampaignName"></span>'+
+    '</form>'
+
+  beforeEach(function() {
+    var container = document.createElement('div');
+    container.setAttribute('id',rootId);
+    document.body.appendChild(container);
+    container.innerHTML = authenticationFormMarkup;
+    authenticationForm = new AuthenticationForm();
   });
 
-  it("Case Input not Empty", function(){
-    window.$ = function(v) {
-      return {
-        val: function() {
-          return "New Campaign Name";
-        }
-      }
-    }
-    var expected = "";
-    var result = validateCampaignName();
-    expect(result).toEqual(expected);
+  it("Blank campaign name", function() {
+    expect(authenticationForm.txtCampaignName).toEqual('');
   });
 
-})
+  it("submit require campaign name", function() {
+     authenticationForm.create();
+     expect(authenticationForm.lblErrorCampaignName).toEqual('Please input campaign name.');
+     expect($("#lblErrorCampaignName").text()).toEqual('Please input campaign name.');
+  });
+
+});
+
