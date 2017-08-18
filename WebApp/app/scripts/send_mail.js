e501a075746ba218ac2bb980a348752b14d76349
agileman <prapapan.tiyasaengthong@dtac.co.th>
2017-08-18 12:52:31 +0700

    Load page send email with passing campaignId

--- WebApp/app/scripts/send_mail.js
+++ WebApp/app/scripts/send_mail.js
@@ -1,70 +1,112 @@
 function submitForm()
+{
+  var fromVal=document.forms['form1']['from'].value;
+  var toVal=document.forms['form1']['to'].value;
+  var subjectVal=document.forms['form1']['subject'].value;
+  var bodyVal=document.forms['form1']['emailBody'].value;
+
+  var result = true;
+
+
+  if (fromVal==null || fromVal=='')
     {
-    var fromVal=document.forms['form1']['from'].value;
-    var toVal=document.forms['form1']['to'].value;
-    var subjectVal=document.forms['form1']['subject'].value;
-    var bodyVal=document.forms['form1']['emailBody'].value;
-
-    var result = true;
-
-
-    if (fromVal==null || fromVal=='')
-      {
-      //document.getElementById("fromError").innerHTML = "Please fill FROM value";
-      $('#fromError').html('Please fill FROM value');
-      result = false;
-      }
-    if (toVal=='' || toVal==null)
-      {
-      //document.getElementById("toError").innerHTML = "Please fill TO value";
-      $('#toError').html('Please fill TO value');
-      result = false;
-      }
-    if (subjectVal==null || subjectVal=='')
-      {
-      $('#subjectError').html('Please fill SUBJECT value');
-      //document.getElementById("subjectError").innerHTML = "Please fill SUBJECT value";
-      result = false;
-      }
-
-    if (bodyVal==null || bodyVal=='')
-      {
-      $('#bodyError').html('Please fill BODY value');
-     // document.getElementById("bodyError").innerHTML = "Please fill BODY value";
-      result = false;
-      }
-
-    if (result == true ) {
-      // TODO: Call backend sendMail
-      alert('Sending');
-      var data = [{
-          'fromEmail' : $('#from').val(),
-          'toEmail' : $('#to').val(),
-          'ccEmail' : $('#cc').val(),
-          'subject' : $('#subject').val(),
-          'body' : $('#emailBody').val()
-      }];
-      console.log(data)
-       $.ajax({
-           type: 'POST',
-           url: 'http://ec2-52-77-254-50.ap-southeast-1.compute.amazonaws.com:8090/email/send/',
-           data: JSON.stringify(data),
-           contentType: 'application/json; charset=utf-8',
-           crossDomain: true,
-           dataType: 'json',
-           success: function (data, status, jqXHR) {
-               alert('Send mail success');
-           },
-
-           error: function (jqXHR, status) {
-               console.log(jqXHR);
-               alert('Send mail fail' + status.code);
-           }
-       });
+    //document.getElementById("fromError").innerHTML = "Please fill FROM value";
+    $('#fromError').html('Please fill FROM value');
+    result = false;
     }
-    return result;
-
+  if (toVal=='' || toVal==null)
+    {
+    //document.getElementById("toError").innerHTML = "Please fill TO value";
+    $('#toError').html('Please fill TO value');
+    result = false;
     }
+  if (subjectVal==null || subjectVal=='')
+    {
+    $('#subjectError').html('Please fill SUBJECT value');
+    //document.getElementById("subjectError").innerHTML = "Please fill SUBJECT value";
+    result = false;
+    }
+
+  if (bodyVal==null || bodyVal=='')
+    {
+    $('#bodyError').html('Please fill BODY value');
+   // document.getElementById("bodyError").innerHTML = "Please fill BODY value";
+    result = false;
+    }
+
+  if (result == true ) {
+    // TODO: Call backend sendMail
+    alert('Sending');
+    var data = {
+        'fromEmail' : $('#from').val(),
+        'toEmail' : $('#to').val(),
+        'ccEmail' : $('#cc').val(),
+        'subject' : $('#subject').val(),
+        'body' : $('#emailBody').val()
+    };
+    console.log(data)
+     $.ajax({
+         type: 'POST',
+         url: 'http://ec2-52-77-254-50.ap-southeast-1.compute.amazonaws.com:8090/email/send/',
+         data: JSON.stringify(data),
+         contentType: 'application/json; charset=utf-8',
+         crossDomain: true,
+         dataType: 'json',
+         success: function (data, status, jqXHR) {
+             alert('Send mail success');
+         },
+
+         error: function (jqXHR, status) {
+             console.log(jqXHR);
+             alert('Send mail fail' + status.code);
+         }
+     });
+  }
+  return result;
+
+}
+
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return decodeURI(results[1]) || 0;
+    }
+}
+
+var CampaignInfo = function(name){
+     $.ajax({
+         type: 'GET',
+         url: 'http://ec2-52-77-254-50.ap-southeast-1.compute.amazonaws.com:8090/campaignmaster/' + $.urlParam(name),
+         crossDomain: true,
+         success: function (response) {
+             //response = JSON.stringify(response);
+             var responseData = JSON.parse(response);
+             console.log(response);
+         },
+
+//         error: function (jqXHR, status) {
+//             console.log(jqXHR);
+//             alert('Send mail fail' + status.code);
+//
+//         }
+     });
+
+}
+
+var SendMailForm = function() {
+    var hasError = false;
+    //var campaignInfo = $.trim($.urlParam('campaignId'));
+    var campaignInfo = CampaignInfo('campaignId');
+    //this.txtCampaignName = $.trim($.urlParam('campaignId'));
+    // this.ddlEmailGroup = $.trim($("#ddlEmailGroup").val());
+    this.from = campaignInfo['fromEmail'];
+    this.to = campaignInfo['emailGroup'];
+    this.subject = campaignInfo['emailSubject'];
+}
+
 function openCity(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName('tabcontent');
