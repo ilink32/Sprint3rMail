04d3f88d6eb528473379ee59a03514a3302edad6
odde <odde@oddes-Mac-mini.local>
2017-08-18 12:44:26 +0700

    add EmailRepository.java

---
+++ AppServe/src/main/java/th/co/sprint3r/mail/dao/EmailRepository.java
@@ -0,0 +1,21 @@
+package th.co.sprint3r.mail.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import th.co.sprint3r.mail.model.Email;
+
+@Repository
+public class EmailRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public int insert(Email email){
+
+        String sql = "INSERT INTO send_history " +
+                "(send_from, send_to, send_subject, send_body, send_cc, send_time) VALUES (?, ?, ?, ?, ?, NOW())";
+
+        return jdbcTemplate.update(sql, new Object[] { email.getFromEmail(), email.getToEmail(), email.getSubject(), email.getBody(), email.getCcEmail()});
+    }
+}
